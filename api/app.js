652e59d52768dccc4dfaa5e3920dd8cbd87c6b7e
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-03-14 16:09:11 +0100

    :sparkles: app.js init config

---
+++ api/app.js
@@ -0,0 +1,67 @@
+// express path
+const path = require('path');
+// express framework
+const express = require('express');
+// bodyParser to parse req
+const bodyParser = require('body-parser');
+// Mongoose for the db
+const mongoose = require('mongoose');
+// Multer for file management
+const multer = require('multer');
+
+// ROUTES
+const authRoute = require('./routes/auth.route');
+
+//------------------------------
+// APP INIT
+//------------------------------
+
+const app = express();
+
+//------------------------------
+// FILES
+//------------------------------
+
+//------------------------------
+// APP CONFIG
+//------------------------------
+app.use(bodyParser.json());
+// Headers
+app.use((req, res, next) =>{
+    // cors
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // methods
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    // allowed headers
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+//------------------------------
+// ROUTES
+//------------------------------
+app.use('/auth', authRoute);
+
+//------------------------------
+// ERRORS MANAGEMENT
+//------------------------------
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+//------------------------------
+// DB CONNECTION
+//------------------------------
+mongoose
+    .connect('mongodb+srv://benjamin:TfvElkidhn9ZEQnV@senpai-ty4s4.mongodb.net/messages?retryWrites=true',  { useNewUrlParser: true })
+    .then(res =>{
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
